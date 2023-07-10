6f96575649372a9820a724566226404fafb568ae
Mas5ive <empty>
2023-07-10 17:29:22 +0300

    feat: define sub-applications

---
+++ my_notes/sub_apps.py
@@ -0,0 +1,22 @@
+from data.user import UserData
+from prompt_toolkit.application import Application
+
+
+def view(data: UserData, note_num: int) -> Application:
+    pass
+
+
+def editor(data: UserData, note_num: int) -> Application:
+    pass
+
+
+def deleter(data: UserData, note_num: int) -> Application:
+    pass
+
+
+def factory(data: UserData, note_num: int) -> Application:
+    pass
+
+
+def gallary(data: UserData, *args) -> Application:
+    pass
